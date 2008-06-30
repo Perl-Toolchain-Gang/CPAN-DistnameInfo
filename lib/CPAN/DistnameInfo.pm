762e25652dc6baadd5e2eed8ce4609148a73d46d
Ruslan Zakirov via RT <bug-CPAN-DistnameInfo@rt.cpan.org>
2008-06-30 08:24:13 -0400

    add .tar.bz2 as possible extension of files
    Mon Jun 30 08:24:12 2008: Request 37234 was acted upon.
    Transaction: Ticket created by RUZ
           Queue: CPAN-DistnameInfo
         Subject: [PATCH] add .tar.bz2 as possible extension of files
       Broken in: 0.06
        Severity: Important
           Owner: Nobody
      Requestors: RUZ@cpan.org
          Status: new
     Ticket <URL: http://rt.cpan.org/Ticket/Display.html?id=37234 >
    
    There are several distributions on the CPAN that use this extension. We
    use your module in CPAN2RT importer, so it's pretty important.
    
    --
    Best regards, Ruslan.
    
    Signed-off-by: Graham Barr <gbarr@pobox.com>

--- lib/CPAN/DistnameInfo.pm
+++ lib/CPAN/DistnameInfo.pm
@@ -65,7 +65,7 @@ sub new {
   ($info{filename} = $distfile) =~ s,^(((.*?/)?authors/)?id/)?([A-Z])/(\4[A-Z])/(\5[-A-Z0-9]*)/,,
     and $info{cpanid} = $6;
 
-  if ($distfile =~ m,([^/]+)\.(tar\.g?z|zip|tgz)$,i) { # support more ?
+  if ($distfile =~ m,([^/]+)\.(tar\.(?:g?z|bz2)|zip|tgz)$,i) { # support more ?
     $info{distvname} = $1;
     $info{extension} = $2;
   }
