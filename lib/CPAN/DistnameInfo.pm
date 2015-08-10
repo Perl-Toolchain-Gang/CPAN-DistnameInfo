288a43b8c06a2bce99289012948108e86b2a3896
Ricardo Signes <rjbs@cpan.org>
2015-08-10 09:08:47 -0400

    add .tar.xz to recognized extensions

--- lib/CPAN/DistnameInfo.pm
+++ lib/CPAN/DistnameInfo.pm
@@ -85,7 +85,7 @@ sub new {
   ($info{filename} = $distfile) =~ s,^(((.*?/)?authors/)?id/)?([A-Z])/(\4[A-Z])/(\5[-A-Z0-9]*)/,,
     and $info{cpanid} = $6;
 
-  if ($distfile =~ m,([^/]+)\.(tar\.(?:g?z|bz2)|zip|tgz)$,i) { # support more ?
+  if ($distfile =~ m,([^/]+)\.(tar\.(?:g?z|bz2|xz)|zip|tgz)$,i) { # support more ?
     $info{distvname} = $1;
     $info{extension} = $2;
   }
